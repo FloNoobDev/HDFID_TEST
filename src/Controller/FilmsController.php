f48a25f63677b5e0b9fb31ba20fa6219e9aff5cd
FloNoobDev <77100958+FloNoobDev@users.noreply.github.com>
2023-07-04 17:46:37 +0200

    wip

--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Form\MoviesType;
+use App\Form\ClearSessionType;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,7 @@ class FilmsController extends AbstractController
 {
     public function __construct(
         private HttpClientInterface $client,
+        private RequestStack $requestStack,
     ) {
     }
 
@@ -53,26 +56,43 @@ class FilmsController extends AbstractController
         return $jsonRaw;
     }
 
-    #[Route('/films', name: 'films')]
+    #[Route('/', name: 'films')]
     public function index(Request $request): Response
     {
+        $clearSessionForm = $this->createForm(ClearSessionType::class);
+        $clearSessionForm->handleRequest($request);
+
         $moviesForm = $this->createForm(MoviesType::class);
         $movies = $this->GetPopularMovies();
 
         $moviesForm->handleRequest($request);
 
+        $session = $this->requestStack->getSession();
+
+        if ($clearSessionForm->isSubmitted() && $clearSessionForm->isValid()) {
+            $session->clear();
+        }
+
+        if ($session->get('title')) {
+            $movies = $this->GetResultsByName($session->get('title'), $session->get('language'));
+        }
+
         if ($moviesForm->isSubmitted() && $moviesForm->isValid()) {
             $formData = $moviesForm->getData();
 
-            if($formData['honeypot']){
+            if ($formData['honeypot']) {
                 $this->redirectToRoute('index');
             }
+
+            $session->set('title', $formData['title']);
+            $session->set('language', $formData['language']);
 
             $movies = $this->GetResultsByName($formData['title'], $formData['language']);
         }
 
         return $this->render('films/index.html.twig', [
             'moviesForm' => $moviesForm->createView(),
+            'clearSessionForm' => $clearSessionForm->createView(),
             'movies' => $movies,
         ]);
     }
