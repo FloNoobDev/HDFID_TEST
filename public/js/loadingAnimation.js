40a9ec90e5a1b43f7dfe8045bb20fe5f5bc3a668
FloNoobDev <77100958+FloNoobDev@users.noreply.github.com>
2023-07-05 21:30:19 +0200

    Patch loading display

--- public/js/loadingAnimation.js
+++ public/js/loadingAnimation.js
@@ -1,13 +1,10 @@
 
-let objectTimer;
-
-triggerAnimation();
-
-function triggerAnimation() {
-    objectTimer = setTimeout(showPage, 2000);
-}
-
 function showPage() {
     document.getElementById("loader").classList.toggle('d-none');
     document.getElementById("mainDiv").classList.toggle('d-none');
 }
+
+
+window.addEventListener('load',(evt)=>{
+    showPage();
+})
